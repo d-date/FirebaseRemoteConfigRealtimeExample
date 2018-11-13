86271c26b265f8b31d74d6188afc8b0175e1b3ab
Daiki Matsudate <d.matsudate@gmail.com>
2018-11-14 01:41:06 +0900

    Initialize project

---
+++ RemoteConfigRealtimeExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RemoteConfigRealtimeExample
+//
+//  Created by Daiki Matsudate on 2018/11/14.
+//  Copyright © 2018 Daiki Matsudate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
