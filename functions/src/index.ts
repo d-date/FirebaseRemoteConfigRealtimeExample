21e73f0ffbfb66c7d3071603600f57f6ee982e20
Daiki Matsudate <d.matsudate@gmail.com>
2018-11-15 11:09:29 +0900

    refactor

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,23 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
+admin.initializeApp();
+
 exports.pushConfig = functions.remoteConfig.onUpdate(versionMetadata => {
   // Create FCM payload to send data message to PUSH_RC topic.
-  const payload = {
-    topic: "PUSH_RC",
+  const payload: admin.messaging.MessagingPayload = {
     data: {
-      "CONFIG_STATE": "STALE"
+      'CONFIG_STATE': 'STALE'
     }
   };
+
+  const options: admin.messaging.MessagingOptions = {
+    contentAvailable: true
+  }
   // Use the Admin SDK to send the ping via FCM.
-  return admin.messaging().send(payload).then(resp => {
+  return admin.messaging()
+  .sendToTopic('PUSH_RC', payload, options)
+  .then(resp => {
     console.log(resp);
     return null;
   });
