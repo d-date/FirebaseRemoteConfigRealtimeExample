d13956d9758a4b7f949ea0c494a91c1d3b67b5fb
Daiki Matsudate <d.matsudate@gmail.com>
2018-11-15 11:47:15 +0900

    make views

--- RemoteConfigRealtimeExample/AppDelegate.swift
+++ RemoteConfigRealtimeExample/AppDelegate.swift
@@ -7,40 +7,51 @@
 //
 
 import UIKit
+import Firebase
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+  var window: UIWindow?
 
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+    Messaging.messaging().delegate = self
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+    UNUserNotificationCenter.current().requestAuthorization(
+      options: authOptions,
+      completionHandler: {_, _ in })
+
+    application.registerForRemoteNotifications()
+    return true
+  }
+
+  // Silent push notification
+  func application(_ application: UIApplication,
+                   didReceiveRemoteNotification userInfo: [AnyHashable : Any],
+                   fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+    print("Entire message \(userInfo)")
+
+    if let _ = userInfo["CONFIG_STATE"] as? String {
+      UserDefaults.standard.set(true, forKey: "CONFIG_STALE")
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
+    NotificationCenter.default.post(name: .init("stale"), object: nil, userInfo: userInfo)
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+    completionHandler(.newData)
+  }
 }
 
+extension AppDelegate : MessagingDelegate {
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+    Messaging.messaging().subscribe(toTopic: "PUSH_RC") { error in
+      print("Subscribed to PUSH_RC topic")
+    }
+  }
+
+  func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) {
+    print("Received data message: \(remoteMessage.appData)")
+  }
+}
--- RemoteConfigRealtimeExample/ViewController.swift
+++ RemoteConfigRealtimeExample/ViewController.swift
@@ -7,14 +7,87 @@
 //
 
 import UIKit
+import Firebase
 
+@objc(ViewController)
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  let welcomeMessageConfigKey = "welcome_message"
+  let welcomeMessageCapsConfigKey = "welcome_message_caps"
+  let loadingPhraseConfigKey = "loading_phrase"
+
+  var remoteConfig: RemoteConfig!
+  @IBOutlet weak var welcomeLabel: UILabel!
+  @IBOutlet weak var fetchButton: UIButton!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    remoteConfig = RemoteConfig.remoteConfig()
+    remoteConfig.setDefaults(fromPlist: "RemoteConfigDefaults")
+    fetchConfig()
+
+    NotificationCenter.default.addObserver(self, selector: #selector(fetchConfig), name: .init("stale"), object: nil)
+  }
+
+  @objc func fetchConfig() {
+    welcomeLabel.text = remoteConfig[loadingPhraseConfigKey].stringValue
+
+    var expirationDuration = 43200 // 12 hours in seconds.
+    // If your app is using developer mode or cache is stale, cacheExpiration is set to 0,
+    // so each fetch will retrieve values from the service.
+
+    if remoteConfig.configSettings.isDeveloperModeEnabled || UserDefaults.standard.bool(forKey: "CONFIG_STALE") {
+      expirationDuration = 0
+      UserDefaults.standard.set(false, forKey: "CONFIG_STALE")
     }
 
+    remoteConfig.fetch(withExpirationDuration: TimeInterval(expirationDuration)) { (status, error) -> Void in
 
+      let alertAction: UIAlertAction = .init(title: "OK", style: .default, handler: nil)
+
+      switch status {
+      case .success:
+        print("Config fetched!")
+        let alertController: UIAlertController = .init(title: "Fetch Success", message: nil, preferredStyle: .alert)
+        alertController.addAction(alertAction)
+        self.present(alertController, animated: true, completion: nil)
+
+        // After config data is successfully fetched, it must be activated before newly fetched
+        // values are returned.
+        self.remoteConfig.activateFetched()
+      case .noFetchYet:
+        break
+      case .throttled:
+        print("Config throttled")
+        let alertController: UIAlertController = .init(title: "Fetch Throttled", message: nil, preferredStyle: .alert)
+        alertController.addAction(alertAction)
+        self.present(alertController, animated: true, completion: nil)
+
+        print("Error: \(error?.localizedDescription ?? "No error available.")")
+      case .failure:
+        print("Config not fetched")
+        let alertController: UIAlertController = .init(title: "Fetch Failed", message: nil, preferredStyle: .alert)
+        alertController.addAction(alertAction)
+        self.present(alertController, animated: true, completion: nil)
+
+        print("Error: \(error?.localizedDescription ?? "No error available.")")
+
+      }
+      self.displayWelcome()
+    }
+  }
+
+  func displayWelcome() {
+    var welcomeMessage = remoteConfig[welcomeMessageConfigKey].stringValue
+
+    if remoteConfig[welcomeMessageCapsConfigKey].boolValue {
+      welcomeMessage = welcomeMessage?.uppercased()
+    }
+    welcomeLabel.text = welcomeMessage
+  }
+
+  @IBAction func handleFetchTouch(_ sender: AnyObject) {
+    fetchConfig()
+  }
 }
-
